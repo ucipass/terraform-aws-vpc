c915c949261040927537fc0d499164839cf5b7ca
ucipass@gmail.com <ucipass@gmail.com>
2021-09-04 16:33:18 -0500

    0.0.2

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 variable "vpc_name" {
   type = string
-  default = "AA1"
+  description = "Default prefix used to name the VPN and other resources associated with it"
+  default = "AA"
 }
 
 variable "vpc_cidr" {
   type = string
+  description = "CIDR block for the VPN currently only /16 is supported as /24 will be reserved for the subnets"
   default = "10.1.0.0/16"
 }
 
